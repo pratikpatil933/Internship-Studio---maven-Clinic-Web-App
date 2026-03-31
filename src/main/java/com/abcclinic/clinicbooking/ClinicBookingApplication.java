e1cf28b040c4425481ac4440318daee7c038f4fa
Pratik Patil <51234441+pratikpatil933@users.noreply.github.com>
2026-03-31 14:02:44 +0530

    Update ClinicBookingApplication.java

--- src/main/java/com/abcclinic/clinicbooking/ClinicBookingApplication.java
+++ src/main/java/com/abcclinic/clinicbooking/ClinicBookingApplication.java
@@ -2,17 +2,19 @@ package com.abcclinic.clinicbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder; // Need this import
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // Need this import
 
 @SpringBootApplication
-public class ClinicBookingApplication {
+public class ClinicBookingApplication extends SpringBootServletInitializer { // Added 'extends'
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicBookingApplication.class, args);
     }
 
-	/*
-	 * @Override protected SpringApplicationBuilder
-	 * configure(SpringApplicationBuilder builder) { return
-	 * builder.sources(ClinicBookingApplication.class); }
-	 */
+    // This method is mandatory for external Tomcat deployment
+    @Override 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+        return builder.sources(ClinicBookingApplication.class); 
+    }
 }
